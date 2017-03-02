f9c16d60048e6c2ef1bf3c270a514b540b94fbab
songyijian <397032724@qq.com>
2017-03-02 19:36:25 +0800

    cli

---
+++ Vue/cli1/src/components/V.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Vv:</p>
+</template>
+
+<script>
+    export default {
+        name: 'v',
+        data() {
+            return {
+                //name: 'TEST1'
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ Vue/cli1/src/router/index.js
@@ -0,0 +1,20 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+import V from '@/components/V'
+
+Vue.use(Router)
+
+export default new Router({
+    routes: [{
+            path: '/',
+            name: 'Hello',
+            component: Hello
+        },
+        {
+            path: '/v',
+            name: 'V',
+            component: V
+        }
+    ]
+})
