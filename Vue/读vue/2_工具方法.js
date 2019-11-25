c2a6c4223cfe4a7f97fb1c7b83e1fd947add4247
yjsong <yj.song@sigmob.com>
2019-11-25 20:01:24 +0800

    读vue

---
+++ Vue/读vue/2_工具方法.js
@@ -0,0 +1,19 @@
+function makeMap(
+  str,
+  expectsLowerCase
+) {
+  var map = Object.create(null);
+  var list = str.split(',');
+  for (var i = 0; i < list.length; i++) {
+    map[list[i]] = true;
+  }
+  console.log(map);
+  return expectsLowerCase
+    ? function (val) { return map[val.toLowerCase()]; }
+    : function (val) { return map[val]; }
+}
+
+
+makeMap('slot,component', true)("SLOT") //true
+makeMap('slot,component')("slot") //true
+
